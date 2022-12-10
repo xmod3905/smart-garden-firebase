c66f135546d4ec9a55bd45a1a38d09f1809b8fad
Muhammad Iqbal <muh.iqbalg1@gmail.com>
2022-12-11 00:24:43 +0700

    DEL: temp.sh

--- temp.sh
+++
@@ -1,7 +0,0 @@
-echo "# smart-garden-firebase" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/xmod3905/smart-garden-firebase.git
-git push -u origin main
